87f18b812bd946614652ddcddbb85255c57bdc1a
Nik Cubrilovic <nc9@protonmail.com>
2020-05-27 11:52:11 +1000

    add BASE_PATH to settings

--- geoprocess/settings.py
+++ geoprocess/settings.py
@@ -9,3 +9,5 @@ REDIS_HOST = os.environ.get("REDIS_HOST", default="127.0.0.10")
 GOOGLE_PLACES_API_KEY = os.environ.get("GOOGLE_PLACES_API_KEY")
 
 MONGO_CONNECT_URL = os.environ.get("MONGO_CONNECT_URL")
+
+BASE_PATH = os.path.dirname(os.path.dirname(__file__))
